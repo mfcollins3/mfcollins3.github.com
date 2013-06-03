e981ed65151996d29fcc813b5080c926087f779a
Michael Collins <michael@imaginaryrealities.com>
2013-06-02 21:45:12 -0700

    I removed the tree and replaced it with a navigation list.

--- javascript/blog.js
+++ javascript/blog.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$.jstree._themes="/javascript/jstree/themes/";$("#categoryTree").jstree({core:{},plugins:["json_data","themes"],json_data:{ajax:{url:"/data/categories.json"}},themes:{theme:"default"}})});
+$(document).ready(function(){});
